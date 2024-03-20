2811ce4329e72c2058164639cc02e7de03cbfc02
Âu Ngọc Diệp <91660142+diepAu13122001@users.noreply.github.com>
2024-03-20 14:05:23 +0700

    create project

---
+++ money-management-be/src/main/java/com/diepau/moneymanagementbe/MoneyManagementBeApplication.java
@@ -0,0 +1,13 @@
+package com.diepau.moneymanagementbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyManagementBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyManagementBeApplication.class, args);
+	}
+
+}
